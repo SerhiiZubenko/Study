6fc123b9a7ca6ac4f6cc8d22d3f02c08c3e9d660
Serhii Zubenko <Serhii_Zubenko@epam.com>
2022-04-05 17:09:53 +0300

    HW1_Automation

---
+++ HW1_A/src/main/java/Program.java
@@ -0,0 +1,14 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://selenium.dev");
+        driver.quit();
+    }
+}
